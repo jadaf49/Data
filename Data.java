882f8692d88a277fca992247238106dbbb5e2af4
jadaf49 <jadaf49@nycstudents.net>
2025-05-04 17:08:41 +0000

    first commit

---
+++ Data.java
@@ -0,0 +1,20 @@
+public class Data
+{
+    public static final int MAX = Integer.MAX_VALUE;
+    private int[][] grid;
+
+    public Data()
+    {
+
+    }
+
+    public void repopulate()
+    {
+
+    }
+
+    public int countIncreasingCols()
+    {
+
+    }
+}
